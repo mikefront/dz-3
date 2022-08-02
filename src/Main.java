cc27fc7effad60ccc01c2c249dfcd4ce9fa1883f
mikefront <mike_front@mail.ru>
2022-08-02 17:14:03 +0500

    задание1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание1
+        int clientOs = 0;
+        if (clientOs == 0) {
+            System.out.println(" у вас IOS ");
+        } else if (clientOs == 1) {
+            System.out.println(" у вас Андроид ");
+        } else {
+            System.out.println(" ОС не найдена ");
+            System.out.println(" коммит и пуш");
+        }
     }
-}
