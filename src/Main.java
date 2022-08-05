1b70c3f3eeb57a419648e02da8465e6c37ef7024
mikefront <mike_front@mail.ru>
2022-08-05 10:27:02 +0500

    задание1

--- src/Main.java
+++ src/Main.java
@@ -10,20 +10,5 @@ public class Main {
             System.out.println("У вас Андроид ");
         } else {
             System.out.println(" ОС не найдена ");
-
-            // Задание2
-            int clientDeviceYear = 2017;
-            if (clientDeviceYear == 0 && clientDeviceYear > 2015) {
-                System.out.println("Установите обычную версию для iOS по ссылке ");
-            } else if (clientDeviceYear == 0 && clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию версию для IOS по ссылке ");
-            } else if (clientDeviceYear == 1 && clientDeviceYear > 2015) {
-                System.out.println("Установите обычную версию для Андроид по ссылке");
-            } else if (clientDeviceYear == 1 && clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию для Андроид по ссылке ");
-            } else {
-                System.out.println("Ваще устройство не поддержиается ");
-                }
-            }
         }
-    }
+    }}
