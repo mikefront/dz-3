86107ed2e27022819e5e64a2f9e798d03bdbf15c
mikefront <mike_front@mail.ru>
2022-08-08 10:48:10 +0500

    задание5 выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
 
         // Задание1
         int clientOs = 1;
@@ -27,10 +26,11 @@ public class Main {
         // Задание3
         int year = 2021;
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println(year + "Год високосный");
+            System.out.println(year + "Год високосный ");
         } else {
-            System.out.println(year + "год не високосный");
+            System.out.println(year + "год не високосный ");
         }
+
         // Задание4
         int deliveryDistance = 95;
         int kilometreLimit = 20;
@@ -40,7 +40,37 @@ public class Main {
         } else {
             deliveryDays = ((deliveryDistance - kilometreLimit) / kilometers) + deliveryDays;
         }
-        System.out.println("Доставка дней" + deliveryDays);
+        System.out.println(" Доставка дней - " + deliveryDays);
+
+        // Задание5
+        int monthNumber = 13;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println(" Зима ");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна ");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето ");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень ");
+                break;
+            default:
+                System.out.println("Такого месяца нет ");
+
         }
+
+    }
+
 
     }
